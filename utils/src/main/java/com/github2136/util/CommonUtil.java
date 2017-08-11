04eaa2f5ef0adc9775ed90df1f80f506969095c8
github2136 <github2136@163.com>
2017-08-11 11:56:32 +0800

    增加String比较

--- utils/src/main/java/com/github2136/util/CommonUtil.java
+++ utils/src/main/java/com/github2136/util/CommonUtil.java
@@ -21,6 +21,14 @@ public class CommonUtil {
         return (int) (px / density + 0.5f);
     }
 
+    public static boolean isEquals(String str1, String str2) {
+        return (str1 == null && str2 == null) || (str1 != null && str2 != null && str1.equals(str2));
+    }
+
+    public static boolean isNotEquals(String str1, String str2) {
+        return !isEquals(str1, str2);
+    }
+
     public static boolean isEmpty(String str) {
         return str == null || str.length() == 0;
     }
