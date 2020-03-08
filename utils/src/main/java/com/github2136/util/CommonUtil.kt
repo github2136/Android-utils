67ea102aa0ecb682cd2c106f6dacf3a2b1b63bf1
github2136 <github2136@163.com>
2020-03-08 16:20:47 +0800

    单位转换

--- utils/src/main/java/com/github2136/util/CommonUtil.kt
+++ utils/src/main/java/com/github2136/util/CommonUtil.kt
@@ -2,7 +2,6 @@ package com.github2136.util
 
 import android.app.AppOpsManager
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 
 /**
@@ -10,14 +9,43 @@ import android.content.pm.PackageManager
  */
 object CommonUtil {
     @JvmStatic
+    fun px2sp(context: Context, sp: Float): Float {
+        val density = context.resources.displayMetrics.scaledDensity
+        return sp / density + 0.5f
+    }
+
+    @JvmStatic
+    fun sp2px(context: Context, sp: Float): Float {
+        val density = context.resources.displayMetrics.scaledDensity
+        return sp * density + 0.5f
+    }
+
+    @JvmStatic
     fun dp2px(context: Context, dp: Float): Int {
         val density = context.resources.displayMetrics.density
         return (dp * density + 0.5f).toInt()
     }
+
     @JvmStatic
-    fun px2dp(context: Context, px: Int): Int {
+    fun px2dp(context: Context, px: Int): Float {
         val density = context.resources.displayMetrics.density
-        return (px / density + 0.5f).toInt()
+        return px / density + 0.5f
+    }
+
+    /**
+     * app是否安装
+     */
+    @JvmStatic
+    fun isAppInstalled(context: Context, packageName: String): Boolean {
+        val pm = context.packageManager
+        val installed: Boolean
+        installed = try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+        return installed
     }
 
     /**
