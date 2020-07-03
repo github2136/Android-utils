713d91e6a7df5908476d989608e1875a1c285b57
github2136 <github2136@163.com>
2020-07-03 16:16:13 +0800

    增加内外网址判断，ip端口判断

--- utils/src/main/java/com/github2136/util/NetworkUtil.kt
+++ utils/src/main/java/com/github2136/util/NetworkUtil.kt
@@ -5,6 +5,8 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import android.os.Build
+import java.net.InetSocketAddress
+import java.net.Socket
 
 /**
  * Created by YB on 2019/5/6
@@ -42,6 +44,57 @@ class NetworkUtil private constructor(context: Context) {
             isLinkable(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE))
         }
 
+    }
+
+    //指定ip端口能否连接
+    fun isHostConnection(host: String, port: Int, timeout: Int = 3000): Boolean {
+        val socket = Socket()
+        try {
+            socket.connect(InetSocketAddress(host, port), timeout)
+        } catch (e: Exception) {
+            return false
+        } finally {
+            try {
+                socket.close()
+            } catch (e: Exception) {
+            }
+        }
+        return true
+    }
+
+    //判断是否是内网地址
+    fun isInnerIP(ipAddress: String): Boolean {
+        val isInnerIp: Boolean
+        val ipNum = getIpNum(ipAddress)
+        /**
+         * 私有IP：A类  10.0.0.0-10.255.255.255
+         * B类  172.16.0.0-172.31.255.255
+         * C类  192.168.0.0-192.168.255.255
+         * 当然，还有127这个网段是环回地址
+         */
+        val aBegin = getIpNum("10.0.0.0")
+        val aEnd = getIpNum("10.255.255.255")
+        val bBegin = getIpNum("172.16.0.0")
+        val bEnd = getIpNum("172.31.255.255")
+        val cBegin = getIpNum("192.168.0.0")
+        val cEnd = getIpNum("192.168.255.255")
+        isInnerIp = isInner(ipNum, aBegin, aEnd) || isInner(ipNum, bBegin, bEnd) || isInner(ipNum, cBegin, cEnd) || ipAddress == "127.0.0.1"
+        return isInnerIp
+    }
+
+    //ip转数字
+    private fun getIpNum(ipAddress: String): Long {
+        val ip = ipAddress.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val a = Integer.parseInt(ip[0]).toLong()
+        val b = Integer.parseInt(ip[1]).toLong()
+        val c = Integer.parseInt(ip[2]).toLong()
+        val d = Integer.parseInt(ip[3]).toLong()
+
+        return a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d
+    }
+
+    private fun isInner(userIp: Long, begin: Long, end: Long): Boolean {
+        return userIp in begin..end
     }
 
     private fun isLinkable(info: NetworkInfo?): Boolean {
